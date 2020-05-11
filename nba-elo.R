dab12890820d919a41a2eaa44b4fb3911c008078
Howard Yong <howardyong@example.com>
2020-05-10 20:51:01 -0500

    data for elo ratings for each season and plot of 2019-2020 szn

--- nba-elo.R
+++ nba-elo.R
@@ -10,10 +10,10 @@ library(foreach)
 library(rvest)
 library(lubridate)
 
-#getwd()
-#setwd('/Users/howardyong/Documents/College/School/Spring2020/SDS323_Spring2020')
+getwd()
+setwd('/Users/howardyong/Documents/College/School/Spring2020/SDS323_Spring2020')
 
-
+#(1)==============================================SCRAPING ONLINE BASKETBALL DATA==============================================
 yearList <- c('2020')
 monthList <- c('october', 'november', 'december', 'january', 'february',
                'march')
@@ -78,28 +78,250 @@ df$home_team_wins <- with(df, ifelse(home_pts > visitor_pts, 1, 0))
 df
 head(df)
 tail(df)
-write.csv(df, './data/nba-scrape-data-2019-2020.csv')
+write.csv(df, 'nba-scrape-data-2019-2020.csv')
 
 
 
-season2016_2017 = read.csv('./data/nba-scrape-data-2016-2017.csv', header=TRUE)
-season2017_2018 = read.csv('./data/nba-scrape-data-2017-2018.csv', header=TRUE)
-season2018_2019 = read.csv('./data/nba-scrape-data-2018-2019.csv', header=TRUE)
-season2019_2020 = read.csv('./data/nba-scrape-data-2019-2020.csv', header=TRUE)
-
-
-
-nbateams <- data.frame(team=union(season2019_2020$home_team_name, season2019_2020$visitor_team_name))
+#(2)==============================================CALCULATING, UPDATING, AND PRODUCING ELO-RATINGS==============================================
+nbateams <- data.frame(team = unique(c(df$home_team_name, df$visitor_team_name)))
 nbateams <- nbateams %>% mutate(elo=1500)
+nbateams$X <- NULL
 nbateams
 
-historical_elo <- data.frame(team = nbateams$team)
-historical_elo <- historical_elo %>% mutate(elo=1500)
+season2016_2017 = read.csv('data/nba-scrape-data-2016-2017.csv')
+season2017_2018 = read.csv('data/nba-scrape-data-2017-2018.csv')
+season2018_2019 = read.csv('data/nba-scrape-data-2018-2019.csv')
+season2019_2020 = read.csv('data/nba-scrape-data-2019-2020.csv')
+head(season2016_2017)
+tail(season2016_2017)
 
-head(season2019_2020)
-season2019_2020$home_team_wins
 
-#Copy and paste season data.frame into loop (seq and match) to update elo for consecutive seasons
+#==============================================2016-2017==============================================
+#Create different data.frames by team<#>
+temp <- paste('team', 1:30, sep="")
+for (i in 1:30) {
+  assign(paste("team",i,sep=""), cbind(rep(0,250)) )
+}
+
+for (i in seq(nrow(season2016_2017))) {
+  match <- season2016_2017[i, ]
+  
+  #Pre-match ratings
+  teamA_elo <- subset(nbateams, team==match$home_team_name)$elo
+  teamB_elo <- subset(nbateams, team==match$visitor_team_name)$elo
+  
+  #Update our ratings
+  new_elo <- elo.calc(wins.A = match$home_team_wins,
+                      elo.A = teamA_elo,
+                      elo.B = teamB_elo,
+                      k = 20)
+  
+  #Results reported as data.frame
+  #Team A's new rating in row1/column1
+  #Team B's new rating in row1/column2
+  new_elo
+  teamA_new_elo <- new_elo[1,1]
+  teamB_new_elo <- new_elo[1,2]
+  
+  #Update the ratings for Teams A and B and leave other teams as they were
+  nbateams <- nbateams %>%
+    mutate(elo = if_else(team==match$home_team_name, teamA_new_elo,
+                         if_else(team==match$visitor_team_name, teamB_new_elo, elo)))
+  
+  home_idx = match(match$home_team_name, nbateams$team)
+  visitor_idx = match(match$visitor_team_name, nbateams$team)
+  temp_home = paste('team', home_idx, sep="")
+  temp_visitor = paste('team', visitor_idx, sep="")
+  update_home = get(temp_home)
+  update_visitor = get(temp_visitor)
+  
+  for (i in 1:length(update_home)) {
+    if (update_home[i,]==0) {
+      update_home[i,] = teamA_new_elo
+      break
+    }
+  }
+  assign(paste('team', home_idx, sep=""), update_home)
+  
+  for (i in 1:length(update_visitor)) {
+    if (update_visitor[i,]==0) {
+      update_visitor[i,] = teamB_new_elo
+      break
+    }
+  }
+  assign(paste('team', visitor_idx, sep=""), update_visitor)
+}
+options(digits=8)
+nbateams %>%
+  arrange(-elo)
+avg_season_elo = 1505
+
+elo_2016_2017 <- sapply(temp, get)
+head(elo_2016_2017)
+tail(elo_2016_2017)
+
+c = 1
+for (team in nbateams$team) {
+  colnames(elo_2016_2017)[c] = team
+  c = c + 1
+}
+tail(elo_2016_2017)
+write.csv(elo_2016_2017, 'elo_2016_2017.csv')
+
+nbateams <- nbateams %>% mutate(elo=0.75*elo+.25*avg_season_elo)
+nbateams %>%
+  arrange(-elo)
+#==============================================2017-2018==============================================
+#Create different data.frames by team<#>
+temp <- paste('team', 1:30, sep="")
+for (i in 1:30) {
+  assign(paste("team",i,sep=""), cbind(rep(0,250)) )
+}
+
+for (i in seq(nrow(season2017_2018))) {
+  match <- season2017_2018[i, ]
+  
+  #Pre-match ratings
+  teamA_elo <- subset(nbateams, team==match$home_team_name)$elo
+  teamB_elo <- subset(nbateams, team==match$visitor_team_name)$elo
+  
+  #Update our ratings
+  new_elo <- elo.calc(wins.A = match$home_team_wins,
+                      elo.A = teamA_elo,
+                      elo.B = teamB_elo,
+                      k = 20)
+  
+  #Results reported as data.frame
+  #Team A's new rating in row1/column1
+  #Team B's new rating in row1/column2
+  new_elo
+  teamA_new_elo <- new_elo[1,1]
+  teamB_new_elo <- new_elo[1,2]
+  
+  #Update the ratings for Teams A and B and leave other teams as they were
+  nbateams <- nbateams %>%
+    mutate(elo = if_else(team==match$home_team_name, teamA_new_elo,
+                         if_else(team==match$visitor_team_name, teamB_new_elo, elo)))
+  
+  home_idx = match(match$home_team_name, nbateams$team)
+  visitor_idx = match(match$visitor_team_name, nbateams$team)
+  temp_home = paste('team', home_idx, sep="")
+  temp_visitor = paste('team', visitor_idx, sep="")
+  update_home = get(temp_home)
+  update_visitor = get(temp_visitor)
+  
+  for (i in 1:length(update_home)) {
+    if (update_home[i,] == 0) {
+      update_home[i,] = teamA_new_elo
+      break
+    }
+  }
+  assign(paste('team', home_idx, sep=""), update_home)
+  
+  for (i in 1:length(update_visitor)) {
+    if (update_visitor[i,] == 0) {
+      update_visitor[i,] = teamB_new_elo
+      break
+    }
+  }
+  assign(paste('team', visitor_idx, sep=""), update_visitor)
+}
+options(digits=8)
+nbateams %>%
+  arrange(-elo)
+avg_season_elo = 1505
+
+elo_2017_2018 <- sapply(temp, get)
+c = 1
+for (team in nbateams$team) {
+  colnames(elo_2017_2018)[c] = team
+  c = c + 1
+}
+write.csv(elo_2017_2018, 'elo_2017_2018.csv')
+
+nbateams <- nbateams %>% mutate(elo=0.75*elo+.25*avg_season_elo)
+nbateams %>%
+  arrange(-elo)
+
+#==============================================2018-2019==============================================
+#Create different data.frames by team<#>
+temp <- paste('team', 1:30, sep="")
+for (i in 1:30) {
+  assign(paste("team",i,sep=""), cbind(rep(0,250)) )
+}
+
+for (i in seq(nrow(season2018_2019))) {
+  match <- season2018_2019[i, ]
+  
+  #Pre-match ratings
+  teamA_elo <- subset(nbateams, team==match$home_team_name)$elo
+  teamB_elo <- subset(nbateams, team==match$visitor_team_name)$elo
+  
+  #Update our ratings
+  new_elo <- elo.calc(wins.A = match$home_team_wins,
+                      elo.A = teamA_elo,
+                      elo.B = teamB_elo,
+                      k = 20)
+  
+  #Results reported as data.frame
+  #Team A's new rating in row1/column1
+  #Team B's new rating in row1/column2
+  new_elo
+  teamA_new_elo <- new_elo[1,1]
+  teamB_new_elo <- new_elo[1,2]
+  
+  #Update the ratings for Teams A and B and leave other teams as they were
+  nbateams <- nbateams %>%
+    mutate(elo = if_else(team==match$home_team_name, teamA_new_elo,
+                         if_else(team==match$visitor_team_name, teamB_new_elo, elo)))
+  
+  home_idx = match(match$home_team_name, nbateams$team)
+  visitor_idx = match(match$visitor_team_name, nbateams$team)
+  temp_home = paste('team', home_idx, sep="")
+  temp_visitor = paste('team', visitor_idx, sep="")
+  update_home = get(temp_home)
+  update_visitor = get(temp_visitor)
+  
+  for (i in 1:length(update_home)) {
+    if (update_home[i,] == 0) {
+      update_home[i,] = teamA_new_elo
+      break
+    }
+  }
+  assign(paste('team', home_idx, sep=""), update_home)
+  
+  for (i in 1:length(update_visitor)) {
+    if (update_visitor[i,] == 0) {
+      update_visitor[i,] = teamB_new_elo
+      break
+    }
+  }
+  assign(paste('team', visitor_idx, sep=""), update_visitor)
+}
+options(digits=8)
+nbateams %>%
+  arrange(-elo)
+avg_season_elo = 1505
+
+elo_2018_2019 <- sapply(temp, get)
+c = 1
+for (team in nbateams$team) {
+  colnames(elo_2018_2019)[c] = team
+  c = c + 1
+}
+write.csv(elo_2018_2019, 'elo_2018_2019.csv')
+
+nbateams <- nbateams %>% mutate(elo=0.75*elo+.25*avg_season_elo)
+nbateams %>%
+  arrange(-elo)
+
+#==============================================2019-2020==============================================
+#Create different data.frames by team<#>
+temp <- paste('team', 1:30, sep="")
+for (i in 1:30) {
+  assign(paste("team",i,sep=""), cbind(rep(0,250)) )
+}
+
 for (i in seq(nrow(season2019_2020))) {
   match <- season2019_2020[i, ]
   
@@ -124,19 +346,57 @@ for (i in seq(nrow(season2019_2020))) {
   nbateams <- nbateams %>%
     mutate(elo = if_else(team==match$home_team_name, teamA_new_elo,
                          if_else(team==match$visitor_team_name, teamB_new_elo, elo)))
+  
+  home_idx = match(match$home_team_name, nbateams$team)
+  visitor_idx = match(match$visitor_team_name, nbateams$team)
+  temp_home = paste('team', home_idx, sep="")
+  temp_visitor = paste('team', visitor_idx, sep="")
+  update_home = get(temp_home)
+  update_visitor = get(temp_visitor)
+  
+  for (i in 1:length(update_home)) {
+    if (update_home[i,] == 0) {
+      update_home[i,] = teamA_new_elo
+      break
+    }
+  }
+  assign(paste('team', home_idx, sep=""), update_home)
+  
+  for (i in 1:length(update_visitor)) {
+    if (update_visitor[i,] == 0) {
+      update_visitor[i,] = teamB_new_elo
+      break
+    }
+  }
+  assign(paste('team', visitor_idx, sep=""), update_visitor)
 }
-
 options(digits=8)
-
 nbateams %>%
   arrange(-elo)
-
 avg_season_elo = 1505
 
-nbateams <- nbateams %>% mutate(elo=0.75*elo+.25*avg_season_elo)
+elo_2019_2020 <- sapply(temp, get)
+c = 1
+for (team in nbateams$team) {
+  colnames(elo_2019_2020)[c] = team
+  c = c + 1
+}
+write.csv(elo_2019_2020, 'elo_2019_2020.csv')
 
-nbateams <- nbateams %>%
-  arrange(-elo)
+names(nbateams)[1] <- paste("Team Name")
+names(nbateams)[2] <- paste("Elo Rating")
+write.csv(nbateams, 'nba-elo-current-2019-2020.csv')
+nbateams = read.csv('data/nba-elo-current-2019-2020.csv')
+seasonelo = read.csv('elo_2019_2020.csv')
 
-names(nbateams)[names(nbateams) == "team"] <- "Team"
-names(nbateams)[names(nbateams) == "elo"] <- "Elo Rating"
+seasonelo$X = NULL
+library(reshape2)
+elo2019_2020 <- melt(seasonelo)
+elo_df <- melt(elo_2019_2020)
+elo_df <- elo_df[(elo_df$Elo.Rating > 0),]
+
+colnames(elo_df) <- c("Season.Progress", "Team", "Elo.Rating")
+ggplot(elo_df, aes(x=Season.Progress, y=Elo.Rating, color=Team)) + geom_line()
+
+  
+  
